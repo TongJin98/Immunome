b1143d9c78051fd273f0d66bec972a257f06d87b
Haoyin Xu <hax042@ucsd.edu>
2019-12-03 18:36:26 -0800

    Revert "Merge branch 'master' of https://github.com/TongJin98/Immunome"
    This reverts commit b8fd15780e5b8aef74499d5b8edb0fc535aed463, reversing
    changes made to 6ec8a43e8cfab548b1204b3f013a19cc8756ee3a.

--- mysite/VDJ_Anchors/urls.py
+++ mysite/VDJ_Anchors/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     #path('test1',views.test1, name='test1'),
-    #path('test2',views.test2, name='test2'),
-    #path('test3',views.test3, name='test3'),
 ]
--- mysite/VDJ_Anchors/views.py
+++ mysite/VDJ_Anchors/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .models import VGene
+from .models import Anchor
 from subprocess import Popen, PIPE, STDOUT
-from .forms import UploadFileForm
 
 
 def index(request):
@@ -25,47 +24,4 @@ def index(request):
     except Exception as e:
                 result = "Fail with exception"
                 return HttpResponse(result)
-
-
-def test2(request):
-    command = ["python3", "/Users/axns/Documents/GitHub/HVP_anchors_generator/anchors_generator.py", "-i", "/Users/axns/Documents/GitHub/HVP_anchors_generator/IMGT", "-o", "/Users/axns/Desktop/untitled folder"]
-
-    try:
-        process = Popen(command, stdout=PIPE, stderr=STDOUT)
-        output = process.stdout.read()
-        exitstatus = process.poll()
-        if (exitstatus==0):
-                result = "Helper message: "
-                return HttpResponse(result + str(output))
-        else:
-                result = "Fail"
-                return HttpResponse(result)
-
-    except Exception as e:
-                result = "Fail with exception"
-                return HttpResponse(result)
-
-
-def test3(request):
-
-    Anchor.main("-i", "/Users/axns/Documents/GitHub/HVP_anchors_generator/IMGT", "-o", "/Users/axns/Desktop/untitled folder")
-
-def upload_file(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('/success/url/')
-    else:
-        form = UploadFileForm()
-    return render(request, 'upload.html', {'form': form})
-
-def parse_genes(request):
-
-
-
-
-
-
 '''
-
