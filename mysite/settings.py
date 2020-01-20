a5ef249bb0e145b508a4c76b9a34702a26160078
root <root@immunome.sdsc.edu>
2020-01-19 18:27:16 -0800

    Added VDJAnchors as an installed app in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,9 +31,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
     
-# Put this back into INSTALLED_APPS later: 'VDJ_Anchors.apps.VdjAnchorsConfig',
 
 INSTALLED_APPS = [
+    'VDJ_Anchors.apps.VdjAnchorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
