ea5ce0d2c8cb8e89377c55279632744c091c4690
Haoyin Xu <hax042@ucsd.edu>
2020-01-31 11:55:44 -0800

    multiple file uploads

--- mysite/VDJ_Anchors/forms.py
+++ mysite/VDJ_Anchors/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Anchor
 
 class UploadFileForm(forms.ModelForm):
+    document = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
     class Meta:
         model = Anchor
-        fields = ('description', 'document', )
+        fields = ('description', 'document')
