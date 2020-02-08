afd73e1f0fbf931ed9fea520c5c497877046c123
Haoyin Xu <hax042@ucsd.edu>
2020-02-07 16:12:48 -0800

    Purge
    Cleared everything for a fresh start.

--- VDJ_Anchors/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VdjAnchorsConfig(AppConfig):
-    name = 'VDJ_Anchors'
--- mysite/views.py
+++
@@ -1,6 +0,0 @@
-#from django.http import HttpResponse
-from django.shortcuts import render, redirect
-
-def home(request):
-    return render(request, 'home.html')
-    #return HttpResponse("This is home.")
